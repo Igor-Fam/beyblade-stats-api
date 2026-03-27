a8a8689831d3035f30f8a19deeb93719554a3103
Igor-Fam <igor.mfam@gmail.com>
2026-03-27 17:08:08 -0300

    fix(web): reset scroll to top on route change

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,23 @@
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Hub from './components/Hub';
 import BattleLogger from './components/BattleLogger';
 import Sidebar from './components/Sidebar';
 import StatsPage from './components/StatsPage';
 import PartDetailPage from './components/PartDetailPage';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div id="app-layout">
         <Sidebar />
         <main id="app-content">
